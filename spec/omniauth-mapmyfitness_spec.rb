9e767a0977e3042607a1fb50fb1382e8e2d5e9e3
Yeeland <yeeland@gmail.com>
2012-12-31 11:05:59 -0600

    tests

--- spec/omniauth-mapmyfitness_spec.rb
+++ spec/omniauth-mapmyfitness_spec.rb
@@ -1,7 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "OmniauthMapmyfitness" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  subject do
+    OmniAuth::Strategies::MapMyFitness.new({})
+  end
+
+  it 'should have the correct name' do
+    subject.options.name.should eq('mapmyfitness')
+  end
+
+  it 'should have the correct access token path' do
+    subject.options.client_options.access_token_path.should eq('/3.1/oauth/access_token')
+  end
+
+  it 'should have the correct authorize path' do
+    subject.options.client_options.authorize_path.should eq('/3.1/oauth/authorize')
+  end
+
+  it 'should have the correct http method' do
+    subject.options.client_options.http_method.should eq(:get)
+  end
+
+  it 'should have the correct request token path' do
+    subject.options.client_options.request_token_path.should eq('/3.1/oauth/request_token')
+  end
+
+  it 'should have the correct scheme' do
+    subject.options.client_options.scheme.should eq(:query_string)
+  end
+
+  it 'should have the correct site' do
+    subject.options.client_options.site.should eq("https://api.mapmyfitness.com")
   end
 end
