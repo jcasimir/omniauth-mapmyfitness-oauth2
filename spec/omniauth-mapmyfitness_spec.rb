0478a4f82fcfe8c1b8152c2e17b273ec7f70ebc7
Jeff Casimir <jeff@casimircreative.com>
2014-01-09 14:14:17 -0700

    Reducing test suite which was just duplicating the implementation

--- spec/omniauth-mapmyfitness_spec.rb
+++ spec/omniauth-mapmyfitness_spec.rb
@@ -5,31 +5,16 @@ describe "OmniauthMapmyfitness" do
     OmniAuth::Strategies::MapMyFitness.new({})
   end
 
-  it 'should have the correct name' do
-    subject.options.name.should eq('mapmyfitness')
+  it 'has a name' do
+    subject.options.name.should be
   end
 
-  it 'should have the correct access token path' do
-    subject.options.client_options.access_token_path.should eq('/3.1/oauth/access_token')
+  [:access_token_path, :authorize_path, :http_method,
+   :request_token_path, :scheme, :site].each do |attribute|
+
+    it "has the #{attribute} defined" do
+      subject.options.client_options.send(attribute).should be
+    end
   end
 
-  it 'should have the correct authorize path' do
-    subject.options.client_options.authorize_path.should eq('/3.1/oauth/authorize')
-  end
-
-  it 'should have the correct http method' do
-    subject.options.client_options.http_method.should eq(:get)
-  end
-
-  it 'should have the correct request token path' do
-    subject.options.client_options.request_token_path.should eq('/3.1/oauth/request_token')
-  end
-
-  it 'should have the correct scheme' do
-    subject.options.client_options.scheme.should eq(:query_string)
-  end
-
-  it 'should have the correct site' do
-    subject.options.client_options.site.should eq("https://api.mapmyfitness.com")
-  end
 end
