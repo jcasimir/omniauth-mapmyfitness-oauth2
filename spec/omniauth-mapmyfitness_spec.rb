3f8285b72a43d985c67709eae70e3407b606658e
Jeff Casimir <jeff@casimircreative.com>
2014-01-09 14:15:11 -0700

    Use let rather than subject

--- spec/omniauth-mapmyfitness_spec.rb
+++ spec/omniauth-mapmyfitness_spec.rb
@@ -1,19 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "OmniauthMapmyfitness" do
-  subject do
+  let(:auth) do
     OmniAuth::Strategies::MapMyFitness.new({})
   end
 
   it 'has a name' do
-    subject.options.name.should be
+    auth.options.name.should be
   end
 
   [:access_token_path, :authorize_path, :http_method,
    :request_token_path, :scheme, :site].each do |attribute|
 
     it "has the #{attribute} defined" do
-      subject.options.client_options.send(attribute).should be
+      auth.options.client_options.send(attribute).should be
     end
   end
 
