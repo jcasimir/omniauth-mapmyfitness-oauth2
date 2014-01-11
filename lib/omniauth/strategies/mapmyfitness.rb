760667edb983e6f25bd893db73be4aaede8dbf41
Jeff Casimir <jeff@casimircreative.com>
2014-01-10 17:17:30 -0700

    Implement user data parser

--- lib/omniauth/strategies/mapmyfitness.rb
+++ lib/omniauth/strategies/mapmyfitness.rb
@@ -1,5 +1,5 @@
 require 'omniauth-oauth2'
-require 'pry'
+require 'omniauth-mapmyfitness-oauth2/user'
 
 module OmniAuth
   module Strategies
@@ -15,35 +15,14 @@ module OmniAuth
         }
       }
 
-      option :token_options, {
-        :grant_type => 'authorization_code',
-      }
+      option :token_options, { :grant_type => 'authorization_code' }
 
-      uid {
-        raw_info['user_id']
-      }
+      uid{ raw_info['user_id'] }
 
-      info do
-        {
-          :name => "#{raw_info['first_name']} #{raw_info['last_name']}",
-          :email => raw_info['email'],
-          :nickname => raw_info['username'],
-          :first_name => raw_info['first_name'],
-          :last_name => raw_info['last_name'],
-          :location => [raw_info['start_city'], raw_info['start_state'], raw_info['start_country']].join(', '),
-          #:image => "http://api.mapmyfitness.com/3.1/users/get_avatar?uid=#{raw_info['user_id']}",
-          :urls => {:mapmyfitness => "http://www.mapmyfitness.com/profile/#{raw_info['username']}"}
-        }
-      end
-
-      extra do
-        {
-          'raw_info' => raw_info
-        }
-      end
+      info{ raw_info }
 
       def raw_info
-        @raw_info = JSON.parse(access_token.get("/v7.0/user/self").body)
+        @raw_info ||= JSON.parse(access_token.get("/v7.0/user/self").body)
       end
     end
   end
